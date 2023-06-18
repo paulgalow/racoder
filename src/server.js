e2dc28f304884197331ef39239abb9f854536bae
Paul Galow <paulgalow@users.noreply.github.com>
2023-06-18 17:06:56 +0200

    Activate keep-alive, improve logging and FFmpeg live stream handling

--- src/server.js
+++ src/server.js
@@ -6,9 +6,15 @@ import { log } from "./utils.js";
 const { HTTP_PORT, INPUT_STREAM } = process.env;
 
 function handleBbcRadio4Extra(req, res) {
+  log(`Incoming request for URL '${req.url}' with method '${req.method}'`);
+  log(`Incoming request headers: ${req.rawHeaders}`);
   res.writeHead(200, { "Content-Type": "audio/mpeg" });
 
   const ffmpegProcess = spawn("ffmpeg", [
+    "-nostdin",
+    "-loglevel",
+    "warning",
+    "-re",
     "-i",
     INPUT_STREAM,
     "-vn",
@@ -20,10 +26,24 @@ function handleBbcRadio4Extra(req, res) {
     "mp3",
     "pipe:1",
   ]);
+  ffmpegProcess.stdout.pipe(res);
+
   log(`Spawned FFmpeg process with PID '${ffmpegProcess.pid}'`);
 
-  ffmpegProcess.on("spawn", () => {
-    ffmpegProcess.stdout.pipe(res);
+  ffmpegProcess.stderr.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ffmpegProcess.on("data", () => {
+    log(
+      `FFmpeg process with PID '${ffmpegProcess.pid} encountered an error: ${error}`
+    );
+  });
+
+  ffmpegProcess.on("error", (error) => {
+    log(
+      `FFmpeg process with PID '${ffmpegProcess.pid} encountered an error: ${error}`
+    );
   });
 
   ffmpegProcess.on("close", (code) => {
@@ -51,25 +71,32 @@ function handleNotFound(req, res) {
   res.end();
 }
 
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/bbc-radio-4-extra":
-      handleBbcRadio4Extra(req, res);
-      break;
-    case "/healthcheck":
-      handleHealthcheck(req, res);
-      break;
-    default:
-      handleNotFound(req, res);
-      break;
+const server = http.createServer(
+  { keepAlive: true, keepAliveInitialDelay: 5000 },
+  (req, res) => {
+    switch (req.url) {
+      case "/bbc-radio-4-extra":
+        handleBbcRadio4Extra(req, res);
+        break;
+      case "/healthcheck":
+        handleHealthcheck(req, res);
+        break;
+      default:
+        handleNotFound(req, res);
+        break;
+    }
   }
-});
+);
 
 server.listen(HTTP_PORT ?? 3000);
+console.log(`Server listening on port ${HTTP_PORT ?? 3000} …`);
 
 process.on("SIGINT", async () => {
   log("Stopping server …");
   server.close(() => {
     process.exit(0);
   });
+  setTimeout(() => {
+    process.exit(1);
+  }, 10000);
 });
