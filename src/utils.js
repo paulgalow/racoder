bfb67161b2d5dbb321d7880f24b76d97f329fac5
Paul Galow <paulgalow@users.noreply.github.com>
2023-09-02 21:44:20 +0200

    Add time zone support for logs

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,18 @@
 export function log(message) {
-  const dateTimeStamp = new Date().toISOString();
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const dateTimeStamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   console.log(`${dateTimeStamp} - ${message}`);
+}
+
+export function getTimeZone() {
+  const now = new Date();
+  return Intl.DateTimeFormat("en", { timeZoneName: "short" })
+    .formatToParts(now)
+    .find((part) => part.type === "timeZoneName").value;
 }
