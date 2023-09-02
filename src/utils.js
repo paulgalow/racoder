504095d13747b6713da6d94446f0b7b8219dcb1a
Paul Galow <paulgalow@users.noreply.github.com>
2023-09-02 19:17:41 +0200

    Remove unnecessary helper function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
-export async function sleep(ms = 100) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export function log(message) {
   const dateTimeStamp = new Date().toISOString();
   console.log(`${dateTimeStamp} - ${message}`);
