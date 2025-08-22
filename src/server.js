f2967e41d2fd6e61e9f35d048ec4843b0b4cbd47
Nick Miller <nick.miller@gatesventures.com>
2025-08-22 10:20:34 -0700

    adds environment variable package

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import http from "node:http";
 import { spawn } from "node:child_process";
+import dotenv from "dotenv";
 import { getTimeZone, log, LOG_LEVELS, validateEnv } from "./utils.js";
+
+dotenv.config();
 
 const { BITRATE, HTTP_PORT, INPUT_STREAM, OUTPUT_PATH } = validateEnv();
 
@@ -20,98 +23,98 @@ OUTPUT_PATHS.forEach((element, index) => {
 });
 
 function handleStream(req, res) {
-  log(`Incoming request for URL '${req.url}' with method '${req.method}'`);
-  log(`Incoming request headers: ${req.rawHeaders}`, LOG_LEVELS.DEBUG);
-  res.writeHead(200, { "Content-Type": "audio/mpeg" });
-  const stream_url = STREAMS[trim_trailing_slash(req.url)];
+    log(`Incoming request for URL '${req.url}' with method '${req.method}'`);
+    log(`Incoming request headers: ${req.rawHeaders}`, LOG_LEVELS.DEBUG);
+    res.writeHead(200, { "Content-Type": "audio/mpeg" });
+    const stream_url = STREAMS[trim_trailing_slash(req.url)];
 
-  const ffmpegProcess = spawn("ffmpeg", [
-    "-nostdin",
-    "-loglevel",
-    "warning",
-    "-re",
-    "-i",
-    stream_url,
-    "-vn",
-    "-c:a",
-    "libmp3lame",
-    "-b:a",
-    BITRATE,
-    "-f",
-    "mp3",
-    "pipe:1",
-  ]);
-  ffmpegProcess.stdout.pipe(res);
+    const ffmpegProcess = spawn("ffmpeg", [
+        "-nostdin",
+        "-loglevel",
+        "warning",
+        "-re",
+        "-i",
+        stream_url,
+        "-vn",
+        "-c:a",
+        "libmp3lame",
+        "-b:a",
+        BITRATE,
+        "-f",
+        "mp3",
+        "pipe:1",
+    ]);
+    ffmpegProcess.stdout.pipe(res);
 
-  log(`Spawned FFmpeg process with PID '${ffmpegProcess.pid}'`);
+    log(`Spawned FFmpeg process with PID '${ffmpegProcess.pid}'`);
 
-  ffmpegProcess.stderr.on("data", (data) => {
-    log(`stdout: ${data}`, LOG_LEVELS.DEBUG);
-  });
+    ffmpegProcess.stderr.on("data", (data) => {
+        log(`stdout: ${data}`, LOG_LEVELS.DEBUG);
+    });
 
-  ffmpegProcess.on("data", (error) => {
-    log(
-      `FFmpeg process with PID '${ffmpegProcess.pid} encountered an error: ${error}`
-    );
-  });
+    ffmpegProcess.on("data", (error) => {
+        log(
+            `FFmpeg process with PID '${ffmpegProcess.pid} encountered an error: ${error}`
+        );
+    });
 
-  ffmpegProcess.on("error", (error) => {
-    log(
-      `FFmpeg process with PID '${ffmpegProcess.pid} encountered an error: ${error}`
-    );
-  });
+    ffmpegProcess.on("error", (error) => {
+        log(
+            `FFmpeg process with PID '${ffmpegProcess.pid} encountered an error: ${error}`
+        );
+    });
 
-  ffmpegProcess.on("close", (code) => {
-    log(
-      `FFmpeg process with PID '${ffmpegProcess.pid}' exited with code ${code}`
-    );
-    res.end();
-  });
+    ffmpegProcess.on("close", (code) => {
+        log(
+            `FFmpeg process with PID '${ffmpegProcess.pid}' exited with code ${code}`
+        );
+        res.end();
+    });
 
-  req.on("close", () => {
-    log(
-      `Quitting FFmpeg process with PID '${ffmpegProcess.pid}' …`,
-      LOG_LEVELS.DEBUG
-    );
-    ffmpegProcess.kill();
-  });
+    req.on("close", () => {
+        log(
+            `Quitting FFmpeg process with PID '${ffmpegProcess.pid}' …`,
+            LOG_LEVELS.DEBUG
+        );
+        ffmpegProcess.kill();
+    });
 }
 
 function handleHealthcheck(req, res) {
-  log("Healthcheck probed", LOG_LEVELS.DEBUG);
-  res.writeHead(200);
-  res.end();
+    log("Healthcheck probed", LOG_LEVELS.DEBUG);
+    res.writeHead(200);
+    res.end();
 }
 
 function handleNotFound(req, res) {
-  log(`404 Invalid URL: '${req.url}'`);
-  res.writeHead(404);
-  res.end();
+    log(`404 Invalid URL: '${req.url}'`);
+    res.writeHead(404);
+    res.end();
 }
 
 function gracefulShutdown(signal) {
-  log(`${signal} received. Stopping server …`);
-  server.close(() => {
-    process.exit(0);
-  });
-  setTimeout(() => {
-    log("Timeout reached. Shutting down server now …");
-    process.exit(1);
-  }, 5000);
+    log(`${signal} received. Stopping server …`);
+    server.close(() => {
+        process.exit(0);
+    });
+    setTimeout(() => {
+        log("Timeout reached. Shutting down server now …");
+        process.exit(1);
+    }, 5000);
 }
 
 const server = http.createServer(
-  { keepAlive: true, keepAliveInitialDelay: 5000 },
-  (req, res) => {
-    const req_url = trim_trailing_slash(req.url);
-    if (req_url in STREAMS) {
-        handleStream(req, res);
-    } else if (req_url == "/healthcheck") {
-        handleHealthcheck(req, res);
-    } else {
-        handleNotFound(req, res);
+    { keepAlive: true, keepAliveInitialDelay: 5000 },
+    (req, res) => {
+        const req_url = trim_trailing_slash(req.url);
+        if (req_url in STREAMS) {
+            handleStream(req, res);
+        } else if (req_url == "/healthcheck") {
+            handleHealthcheck(req, res);
+        } else {
+            handleNotFound(req, res);
+        }
     }
-  }
 );
 
 log(`Server timezone: ${getTimeZone()}`);
